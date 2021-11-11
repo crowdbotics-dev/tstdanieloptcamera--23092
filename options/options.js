7766974cd8d7f9162d8515e456ae094a1275612e
Crowdbotics <engineering@crowdbotics.com>
2021-11-11 15:54:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tstdanieloptcamera__23092",
+  url: "https://tstdanieloptcamera__23092.botics.co",
+  api: "https://tstdanieloptcamera__23092.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
